9450483687be7dd2074758f12af41c929fb4ad99
Vedhanth123 <gvedhanth123@outlook.com>
2026-03-07 08:23:07 +0000

    Added CORS policy using CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import auth, post, user, vote
 
 
@@ -10,7 +10,24 @@ load_dotenv()
 # app = FastAPI(lifespan=lifespan)
 app = FastAPI()
 
+origins = ["https://www.google.com"]
+# origins = ["*"]  # If public
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # what domains which can talk to our api
+    allow_credentials=True,  #
+    allow_methods=["*"],  # we can allow only specific methods
+    allow_headers=["*"],  # we can allow only specific headers
+)
+
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
+
+@app.get("/")
+def test():
+    return "Joker"
